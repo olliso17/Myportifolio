1ff0c983d438fe53495691979b48e82aa583aee4
olliso17 <https://github.com/olliso17/estudoReact>
2022-06-14 00:12:55 -0300

    ajustando o portifolio

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,12 @@ import TextHeader from './components/tittle/TextHeader';
 function App() {
   return (
     <div className="App">
-        <Container className='header'><TextHeader class='textHeader' name='Patricia Silva Oliveira' 
-        linkedin='linkedin' zap='12345' instagram='olli.so17'></TextHeader></Container>
+        <Container className='header'>
+          <Container className='nomeCard'><TextHeader class='textHeader' name='Patricia Silva Oliveira'></TextHeader></Container>
+          <Container className='nomeLinkedin'><TextHeader class='textHeader' name='linke'></TextHeader></Container>
+          <Container className='nomeZap'><TextHeader class='textHeader' name='zap'></TextHeader></Container>
+          <Container className='nomeGit'><TextHeader class='textHeader' name='Git'></TextHeader></Container>
+          </Container>
         <Container className='body'>Body</Container>
         <Container className='footer'>Footer</Container>
 
