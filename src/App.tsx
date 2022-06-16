58cdb029e4167bcaca716ee42ca348cc6bdc74c9
olliso17 <https://github.com/olliso17/estudoReact>
2022-06-16 02:14:19 -0300

    mudanças no desing da página, estudo claymorphism

--- src/App.tsx
+++ src/App.tsx
@@ -9,23 +9,30 @@ import { FaLinkedin, FaGithub, FaWhatsapp } from "react-icons/fa";
 function App() {
   return (
     <div className="App">
-        <Container className='header'>
+        <Container className='container-image'>
             <Card class='card-head'>
                 <img src={require('./image/foto.jpg')} alt="" />
-            </Card>
-            <Card class='card-link'>
+            </Card>   
+        </Container>
+        <Container className='container-link'>
+          <Card class='card-link'>
               <Card class='link'><FaLinkedin/></Card>
               <Card class='link'><FaGithub/></Card>
               <Card class='link'><FaWhatsapp/></Card>
             </Card>
-          
-            
         </Container>
-        <Container className='body'>
+        <Container className='container-aleatorio'>
+          <Card class='card-aleatorio'>
+
+          </Card>
+        </Container>
+        <Container className='container-text'>
           <Card class='text-body'><h1>Patrícia Silva Oliveira</h1></Card>
         </Container>
-        <Container className='footer'>
-
+        <Container className='container-footer'>
+          <Card class='card-footer'>
+            
+          </Card>
         </Container>
 
     </div>
