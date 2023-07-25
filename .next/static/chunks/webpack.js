037973e573d1729ce07119edff966da8f8485b3a
Patricia Silva <patriciasilva@deso-se.com.br>
2023-07-25 11:01:01 -0300

    feat(all): update breakpoints boxs

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "51dece7002116267"; }
+/******/ 		__webpack_require__.h = function() { return "7b0cb9de8b113df9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
