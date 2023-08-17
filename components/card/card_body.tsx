cb85bbbefeb85a5712926197b5304a5cfb487b2a
Patricia <olli.so17@gmail.com>
2023-08-16 22:03:46 -0300

    feat(style): create util myDescription

--- components/card/card_body.tsx
+++ components/card/card_body.tsx
@@ -1,5 +1,6 @@
 import { Card, Center, Text } from "@chakra-ui/react";
 import { ReactNode } from "react";
+import { textBody } from "../../util/myDescription";
 
 type ContainerProps = {
   children: ReactNode;
@@ -9,20 +10,7 @@ export const CardBody = () => {
   return (
     <>
       <Text layerStyle={"textAll"} className={"animated-text"}>
-        Dissatisfied with the area I studied (bachelor's degree in law), I
-        decided to study Programming. I started with Programming Logic,
-        javascript, html, css and made my first web pages. I fell in love with
-        the area without imagining that I would currently be developing
-        back_end, with node.js, or go, or python, or php, creating api rests,
-        using clean arch, testing, clean code, git, docker, working with
-        postgres relational databases ... non-relational moogo... constantly
-        studying good practices, learning front_end with next.js, react.js...
-        learning every day that the possibilities in this area are endless, that
-        learning is not just about it, but business rules, the rules for
-        applying any content to be developed, this is incredible because I don't
-        just learn to program, I learn how a bank app works, for example, a
-        registration app... In short, I fell in love with the area and I
-        continue to learn with pleasure.
+        {textBody}
       </Text>
     </>
   );
--- components/card/card_header.tsx
+++ components/card/card_header.tsx
@@ -1,5 +1,6 @@
-import { Box, Card, Heading, Text } from "@chakra-ui/react";
+import { Heading, Text } from "@chakra-ui/react";
 import { ReactNode } from "react";
+import { textHeader, titleHeader } from "../../util/myDescription";
 
 type ContainerProps = {
   children: ReactNode;
@@ -9,11 +10,10 @@ export const CardHeader = () => {
   return (
     <>
       <Heading layerStyle={"textAll"} className="animated-title">
-        Hi, my name is Patricia Oliveira!
+        {titleHeader}
       </Heading>
       <Text layerStyle={"textAll"} className="animated-text">
-        I would like to demonstrate my study projects to everyone who is
-        accessing my portfolio.
+        {textHeader}
       </Text>
     </>
   );
